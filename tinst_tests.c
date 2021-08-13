2f33e41cb7052bb15fde1f4d3006be45033b805d
Bruno Sa <bruno.vilaca.sa@gmail.com>
2021-08-14 00:28:54 +0100

    add tinst loads tests

--- tinst_tests.c
+++ tinst_tests.c
@@ -11,9 +11,10 @@ bool tinst_tests(){
     goto_priv(PRIV_HS);
     uintptr_t vaddr_f = hs_page_base(VSI_GI);
     uint32_t instruction = 0;
+    uint64_t value = 0xdeadbeef;
 
     TEST_SETUP_EXCEPT();
-    lb(vaddr_f);
+    value = lb(vaddr_f);
     if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
     TEST_ASSERT("correct tinst when executing a lb which results in a lpf",
         excpt.triggered == true && 
@@ -21,5 +22,60 @@ bool tinst_tests(){
         excpt.tinst == TINST_LOAD(instruction)
     );
 
+    TEST_SETUP_EXCEPT();
+    value = lbu(vaddr_f);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a lbu which results in a lpf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_LPF &&
+        excpt.tinst == TINST_LOAD(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = lh(vaddr_f);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a lh which results in a lpf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_LPF &&
+        excpt.tinst == TINST_LOAD(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = lhu(vaddr_f);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a lhu which results in a lpf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_LPF &&
+        excpt.tinst == TINST_LOAD(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    lw(vaddr_f);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    printf("instruction %x \r\n", instruction);
+    TEST_ASSERT("correct tinst when executing a lw which results in a lpf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_LPF &&
+        excpt.tinst == TINST_LOAD(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = lwu(vaddr_f);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a lwu which results in a lpf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_LPF &&
+        excpt.tinst == TINST_LOAD(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = ld(vaddr_f);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    printf("instruction %x \r\n", instruction);
+    TEST_ASSERT("correct tinst when executing a ld which results in a lpf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_LPF &&
+        excpt.tinst == TINST_LOAD(instruction)
+    );
     TEST_END();
 }
