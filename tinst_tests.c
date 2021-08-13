4c13e46ddde5ffaafff792f4ee77190adc2eee65
Bruno Sa <bruno.vilaca.sa@gmail.com>
2021-08-14 00:34:57 +0100

    add tinst amo tests

--- tinst_tests.c
+++ tinst_tests.c
@@ -3,6 +3,7 @@
 
 #define TINST_LOAD(instuction) ((instruction) & 0x7fff)
 #define TINST_STORE(instuction) ((instruction) & 0x1f0707f)
+#define TINST_AMO(instuction) ((instruction) & 0xfff07fff)
 
 bool tinst_tests(){
     
@@ -117,5 +118,168 @@ bool tinst_tests(){
         excpt.cause == CAUSE_SPF &&
         excpt.tinst == TINST_STORE(instruction)
     );
+
+    TEST_SETUP_EXCEPT();
+    value = amoswap_w(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoswap.w which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amoadd_w(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoadd.w which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amoxor_w(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoxor.w which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amoand_w(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoand.w which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amoor_w(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoor.w which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amomin_w(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amomin.w which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amomax_w(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amomax.w which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amominu_w(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amominu.w which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amomaxu_w(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amomaxu.w which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amoswap_d(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoswap.d which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amoadd_d(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoadd.d which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amoxor_d(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoxor.d which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amoand_d(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoand.d which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amoor_d(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amoor.d which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amomin_d(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amomin.d which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amomax_d(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amomax.d which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amominu_d(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amominu.d which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
+    TEST_SETUP_EXCEPT();
+    value = amomaxu_d(vaddr_f,value);
+    if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
+    TEST_ASSERT("correct tinst when executing a amomaxu.d which results in a spf",
+        excpt.triggered == true && 
+        excpt.cause == CAUSE_SPF &&
+        excpt.tinst == TINST_AMO(instruction)
+    );
+
     TEST_END();
 }
