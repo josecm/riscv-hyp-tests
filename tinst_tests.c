895705bb55a2082d2f67cfc0b8d0cf896dc414e1
Jose Martins <josemartins90@gmail.com>
2021-08-14 15:10:57 +0100

    remove printf debugging

--- tinst_tests.c
+++ tinst_tests.c
@@ -54,7 +54,6 @@ bool tinst_tests(){
     TEST_SETUP_EXCEPT();
     lw(vaddr_f);
     if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
-    printf("instruction %x \r\n", instruction);
     TEST_ASSERT("correct tinst when executing a lw which results in a lpf",
         excpt.triggered == true && 
         excpt.cause == CAUSE_LPF &&
@@ -73,7 +72,6 @@ bool tinst_tests(){
     TEST_SETUP_EXCEPT();
     value = ld(vaddr_f);
     if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
-    printf("instruction %x \r\n", instruction);
     TEST_ASSERT("correct tinst when executing a ld which results in a lpf",
         excpt.triggered == true && 
         excpt.cause == CAUSE_LPF &&
@@ -85,7 +83,6 @@ bool tinst_tests(){
     TEST_SETUP_EXCEPT();
     sb(vaddr_f,value);
     if(excpt.triggered) instruction = *((uint16_t*)excpt.epc) | *((uint16_t*)excpt.epc + 2) << 16;
-    printf("instruction %x \r\n", instruction);
     TEST_ASSERT("correct tinst when executing a sb which results in a spf",
         excpt.triggered == true && 
         excpt.cause == CAUSE_SPF &&
