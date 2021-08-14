6b81660743a49ad19c4a654eb699f51f1ac2bbfa
Jose Martins <josemartins90@gmail.com>
2021-08-14 15:14:39 +0100

    clarify tinst test check macros

--- tinst_tests.c
+++ tinst_tests.c
@@ -1,9 +1,9 @@
 #include <rvh_test.h>
 #include <page_tables.h>
 
-#define TINST_LOAD(instuction) ((instruction) & 0x7fff)
-#define TINST_STORE(instuction) ((instruction) & 0x1f0707f)
-#define TINST_AMO(instuction) ((instruction) & 0xfff07fff)
+#define TINST_LOAD(instuction) ((instruction) & (INS_OPCODE | INS_RD | INS_FUNCT3))
+#define TINST_STORE(instuction) ((instruction) & (INS_OPCODE | INS_FUNCT3 | INS_RS2))
+#define TINST_AMO(instuction) ((instruction) & (~TINST_ADDROFF))
 
 bool tinst_tests(){
     
